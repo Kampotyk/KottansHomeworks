309a0fb5db6c0f080066687b53919392aaa49377
Trop <john-outsider@hotmail.com>
2016-06-16 01:07:36 +0300

    Refactored Add method.

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -1,70 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Calculator
 {
     public class StringCalculator
     {
-        public int Add(string numbers)
-        {
-            const string DelimPrefix = "//";
-            const string DefaultDelims = ",\n";
-            const int MaxNumber = 1000;
 
-            int result = 0;
-            int intNumber = 0;
-            string delims = DefaultDelims;
-            string negNumbers = String.Empty;
-            string[] numberArray;
-            string[] delimsArray;
-
-            if (numbers.Length >= DelimPrefix.Length
-                && numbers.Substring(0, DelimPrefix.Length).Equals(DelimPrefix))
+        public int Add(string input)
+        {          
+            if (input.Length == 0)
             {
-                var delimLine = numbers.Substring(DelimPrefix.Length).Split('\n')[0];
-                if (delimLine.Length == 1)
-                {
-                    delimsArray = new string[] { delimLine };
-                }
-                else
-                {
-                    delimsArray = delimLine.Split(new char[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
-                }
-                numbers = numbers.Split('\n')[1];
+                return 0;
+            }
 
-                numberArray = numbers.Split(delimsArray, StringSplitOptions.None);
+            var numbersStr = StringCalculatorHelper.SplitStringToNumbersArray(input);
+            var numbers = StringCalculatorHelper.ParseStringArrayToIntArray(numbersStr);
+            var negNumbers = StringCalculatorHelper.GetNegativeNumbers(numbers);
+
+            if (negNumbers.Length == 0)
+            {
+                return StringCalculatorHelper.GetNumbersSum(numbers);
             }
             else
             {
-                numberArray = numbers.Trim().Split(delims.ToCharArray());
-            }
-
-            if (numbers.Length == 0)
-            {
-                return result;
-            }
-
-            foreach (var strNumber in numberArray)
-            {
-                intNumber = Int32.Parse(strNumber);
-
-                if (intNumber < 0)
-                {
-                    negNumbers = String.Join(", ", Array.FindAll(new[] { negNumbers, strNumber },
-                                                                 number => !String.IsNullOrEmpty(number)));
-                }
-                else
-                {
-                    result += intNumber < MaxNumber ? intNumber : 0;
-                }
-            }
-
-            if (negNumbers.Length > 0)
-            {
                 throw new InvalidOperationException(String.Format("Negatives not allowed: {0}", negNumbers));
             }
-
-            return result;
         }
     }
 }
