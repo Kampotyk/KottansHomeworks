f481f180e616d8b19d04ffce6c07deae55ec3137
Trop <john-outsider@hotmail.com>
2016-06-15 14:03:59 +0300

    Fixed Asserts. Added helper class.

--- StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
@@ -18,49 +18,49 @@ namespace Calculator.Tests
         [TestCase("")]
         public void Add_EmptyString_Test(string value)
         {
-            Assert.That(0, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(0));
         }
 
         [Test]
         [TestCase("1")]
         public void Add_SingleParam_Test(string value)
         {
-            Assert.That(1, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(1));
         }
 
         [Test]
         [TestCase("1,2")]
         public void Add_TwoParam_Test(string value)
         {
-            Assert.That(3, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(3));
         }
 
         [Test]
         [TestCase("1,2,3,4,5")]
         public void Add_MultipleParams_Test(string value)
         {
-            Assert.That(15, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(15));
         }
 
         [Test]
         [TestCase("1\n2")]
         public void Add_NewLinesDelim_Test(string value)
         {
-            Assert.That(3, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(3));
         }
 
         [Test]
         [TestCase("1\n2,3\n4,5")]
         public void Add_MultipleCharDelims_Test(string value)
         {
-            Assert.That(15, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(15));
         }
 
         [Test]
         [TestCase("//;\n1;2")]
         public void Add_PrefixDelim_Test(string value)
         {
-            Assert.That(3, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(3));
         }
 
         [Test]
@@ -75,35 +75,35 @@ namespace Calculator.Tests
         [TestCase("1000")]
         public void Add_SingleThousand_Test(string value)
         {
-            Assert.That(0, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(0));
         }
 
         [Test]
         [TestCase("1,1001,1002")]
         public void Add_MultipleThousands_Test(string value)
         {
-            Assert.That(1, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(1));
         }
 
         [Test]
         [TestCase("//[***]\n1***2***3")]
         public void Add_PrefixStringDelim_Test(string value)
         {
-            Assert.That(6, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(6));
         }
 
         [Test]
         [TestCase("//[*][%]\n1*2%3")]
         public void Add_PrefixMultipleCharDelims_Test(string value)
         {
-            Assert.That(6, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(6));
         }
 
         [Test]
         [TestCase("//[***][%%%][&*%]\n1***2%%%3&*%4")]
         public void Add_PrefixMultipleStringDelims_Test(string value)
         {
-            Assert.That(10, Is.EqualTo(calc.Add(value)));
+            Assert.That(calc.Add(value), Is.EqualTo(10));
         }
     }
 }
